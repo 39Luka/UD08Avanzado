79183804a1e99f7c080a4e9000b9e706141623e6
Cachon <silcaclei@alu.edu.gva.es>
2025-03-10 19:10:20 +0100

    v4

--- src/Generico/Conjunto.java
+++ src/Generico/Conjunto.java
@@ -1,8 +1,5 @@
 package Generico;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class Conjunto<T> {
     private static final  int TAM = 10;
     private T[] array = (T[]) new Object[TAM];
@@ -29,7 +26,37 @@ public class Conjunto<T> {
         boolean found = false;
 
         while (!found && (pos < TAM)){
+            if (array[pos].equals(element)){
+                array[pos] = null;
+                found = true;
+            }
+            pos++;
+        }
+        moverHuecos();
+        return found;
+    }
 
+
+    int buscarHuecos(){
+        int pos = 0;
+        boolean found = false;
+
+        while (!found && (pos < TAM)){
+            if (array[pos] == null) found = true;
+            pos++;
+        }
+        if(!found) pos = -1;
+
+        return pos;
+    }
+
+    void moverHuecos(){
+        int blank = buscarHuecos();
+
+        while (blank != -1){
+            array[blank] = array[blank + 1];
+            array[blank + 1] = null;
+            blank = buscarHuecos();
         }
     }
 }
