d09f7629827057fef7f75aeaf3f265402db952dc
Cachon <silcaclei@alu.edu.gva.es>
2025-03-10 18:44:03 +0100

    v2

---
+++ src/Generico/Conjunto.java
@@ -0,0 +1,18 @@
+package Generico;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Conjunto<T> {
+    private static final  int TAM = 10;
+    private T[] array = (T[]) new Object[TAM];
+
+
+
+    boolean insert(T element){
+
+    }
+
+    boolean delete(T element){
+
+}
